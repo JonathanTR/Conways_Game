e45b8c561cb788340abf7749d0ff51061d399cab
JonathanTR <Jonathan.d.reilly@gmail.com>
2013-11-25 23:51:19 -0500

    append cells to rows

--- game.js
+++ game.js
@@ -14,11 +14,14 @@ renderBoard = function(){
   var container = document.getElementById('container')
   var allCells = board.cells
   for(var rowI = 0; rowI < allCells.length; rowI++){
+    row = document.createElement('div')
+    row.setAttribute('class', 'row')
     for(var cellI = 0; cellI < allCells[rowI].length; cellI++){
       var cell = document.createElement('div')
       cell.setAttribute('class', 'cell')
-      container.appendChild(cell)
+      row.appendChild(cell)
     }
+    container.appendChild(row)
   }
 }
 
