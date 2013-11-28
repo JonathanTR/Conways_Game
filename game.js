9898daccccfdca8fe8cabf49e6e6aba54b9bd4c5
JonathanTR <Jonathan.d.reilly@gmail.com>
2013-11-27 22:43:56 -0500

    Add methods to get neighbors and count living neighbors

--- game.js
+++ game.js
@@ -27,8 +27,50 @@ renderBoard = function(){
   return domBoard
 }
 
+// y-1, x-1  y-1, x  y-1, x+1
+// y, x-1    y,x     y, x+1
+// y+1, x-1  y+1, x  y+1,x+1
+
+var getNeighbors = function(y, x){
+  var modelBoard = conwaysGame.view
+  var neighbors = []
+  neighbors.push(modelBoard[y-1][x-1],
+                 modelBoard[y-1][x],
+                 modelBoard[y-1][x+1],
+                 modelBoard[y][x+1],
+                 modelBoard[y+1][x+1],
+                 modelBoard[y+1][x],
+                 modelBoard[y+1][x-1],
+                 modelBoard[y][x-1]
+                 )
+  return neighbors
+}
+
+var countLiving = function(neighborsArray){
+  var numberOfLivingNeighbors = 0
+  for(var i = 0; i < neighborsArray.length; i++){
+    console.log(neighborsArray[i])
+    if(neighborsArray[i].classList.contains('alive')){
+      numberOfLivingNeighbors += 1
+    }
+  }
+  return numberOfLivingNeighbors
+}
+
+
+boardStepper = function(){
+  for(var y = 0; y < modelBoard.length; y++){
+    for(var x = 0; x < modelBoard[y].length; x++){
+      currentCell = modelBoard[y][x]
+      neighbors = getNeighbors(y,x)
+      livingNeighbors = countLiving(neighbors)
+    }
+  }
+}
+
+
 var conwaysGame = {
-  model: constructBoard(20, 20),
+  model: constructBoard(20, 40),
 }
 
 window.onload = function(){
